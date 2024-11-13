9a2efb1fe444cb86621b4ae00dbe05c99582639c
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-11-13 13:44:57 +0000

    feat:intents (#6)
    * feat:intents
    
    * feat:intents

--- ovos_persona/__init__.py
+++ ovos_persona/__init__.py
@@ -1,6 +1,6 @@
 import json
 import os
-from os.path import dirname
+from os.path import join, dirname
 from typing import Optional, Dict, List, Union
 
 from ovos_bus_client.client import MessageBusClient
@@ -9,10 +9,12 @@ from ovos_config.config import Configuration
 from ovos_config.locations import get_xdg_config_save_path
 from ovos_plugin_manager.persona import find_persona_plugins
 from ovos_plugin_manager.solvers import find_question_solver_plugins
-from ovos_plugin_manager.templates.pipeline import PipelineStageMatcher, IntentHandlerMatch, ConfidenceMatcherPipeline
+from ovos_plugin_manager.templates.pipeline import PipelineStageConfidenceMatcher, IntentHandlerMatch
 from ovos_utils.fakebus import FakeBus
+from ovos_utils.lang import standardize_lang_tag, get_language_dir
 from ovos_utils.log import LOG
 from ovos_workshop.app import OVOSAbstractApplication
+from padacioso import IntentContainer
 
 from ovos_persona.solvers import QuestionSolversService
 
@@ -46,18 +48,57 @@ class Persona:
         return self.solvers.spoken_answer(prompt, lang)
 
 
-class PersonaService(PipelineStageMatcher, OVOSAbstractApplication):
+class PersonaService(PipelineStageConfidenceMatcher, OVOSAbstractApplication):
+    intents = ["ask.intent", "summon.intent"]
+    intent_matchers = {}
+
     def __init__(self, bus: Optional[Union[MessageBusClient, FakeBus]] = None,
                  config: Optional[Dict] = None):
         config = config or Configuration().get("persona", {})
         OVOSAbstractApplication.__init__(
             self, bus=bus or FakeBus(), skill_id="persona.openvoiceos",
             resources_dir=f"{dirname(__file__)}")
-        PipelineStageMatcher.__init__(self, bus, config)
+        PipelineStageConfidenceMatcher.__init__(self, bus, config)
         self.personas = {}
         self.blacklist = self.config.get("persona_blacklist") or []
         self.load_personas(self.config.get("personas_path"))
+        self.active_persona = None
         self.add_event('persona:answer', self.handle_persona_answer)
+        self.add_event('persona:summon', self.handle_persona_summon)
+        self.add_event('persona:release', self.handle_persona_release)
+
+    @classmethod
+    def load_resource_files(cls):
+        intents = {}
+        langs = Configuration().get('secondary_langs', []) + [Configuration().get('lang', "en-US")]
+        langs = set([standardize_lang_tag(l) for l in langs])
+        for lang in langs:
+            intents[lang] = {}
+            locale_folder = get_language_dir(join(dirname(__file__), "locale"), lang)
+            if locale_folder is not None:
+                for f in os.listdir(locale_folder):
+                    path = join(locale_folder, f)
+                    if f in cls.intents:
+                        with open(path) as intent:
+                            samples = intent.read().split("\n")
+                            for idx, s in enumerate(samples):
+                                samples[idx] = s.replace("{{", "{").replace("}}", "}")
+                            intents[lang][f] = samples
+        return intents
+
+    @classmethod
+    def load_intent_files(cls):
+        intent_files = cls.load_resource_files()
+
+        for lang, intent_data in intent_files.items():
+            lang = standardize_lang_tag(lang)
+            cls.intent_matchers[lang] = IntentContainer()
+            for intent_name in cls.intents:
+                samples = intent_data.get(intent_name)
+                if samples:
+                    LOG.debug(f"registering OCP intent: {intent_name}")
+                    cls.intent_matchers[lang].add_intent(
+                        intent_name.replace(".intent", ""), samples)
 
     @property
     def default_persona(self) -> Optional[str]:
@@ -96,15 +137,63 @@ class PersonaService(PipelineStageMatcher, OVOSAbstractApplication):
 
     # Chatbot API
     def chatbox_ask(self, prompt: str, persona: Optional[str] = None, lang: Optional[str] = None) -> Optional[str]:
-        persona = persona or self.default_persona
+        persona = persona or self.active_persona or self.default_persona
         if persona not in self.personas:
             LOG.error(f"unknown persona, choose one of {self.personas.keys()}")
             return None
         messages = [{"role": "user", "content": prompt}]
         return self.personas[persona].chat(messages, lang)
 
-    def match(self, utterances: List[str], lang: Optional[str] = None, message: Optional[Message] = None) -> Optional[IntentHandlerMatch]:
+    # Abstract methods
+    def match_high(self, utterances: List[str], lang: Optional[str] = None,
+                   message: Optional[Message] = None) -> Optional[IntentHandlerMatch]:
         """
+        Recommended before common query
+
+        Args:
+            utterances (list):  list of utterances
+            lang (string):      4 letter ISO language code
+            message (Message):  message to use to generate reply
+
+        Returns:
+            IntentMatch if handled otherwise None.
+        """
+        match = self.intent_matchers[lang].calc_intent(utterances[0].lower())
+
+        if match["name"]:
+            LOG.info(f"Persona exact match: {match}")
+            persona = match["entities"].pop("persona")
+            if match["name"] == "summon":
+                return IntentHandlerMatch(match_type='persona:summon',
+                                          match_data={"persona": persona},
+                                          skill_id="persona.openvoiceos",
+                                          utterance=utterances[0])
+            elif match["name"] == "ask":
+                utterance = match["entities"].pop("query")
+                ans = self.chatbox_ask(utterance,
+                                       lang=lang,
+                                       persona=persona)
+                if ans:
+                    return IntentHandlerMatch(match_type='persona:answer',
+                                              match_data={"answer": ans,
+                                                          "persona": persona},
+                                              skill_id="persona.openvoiceos",
+                                              utterance=utterances[0])
+
+        if self.active_persona and self.voc_match(utterances[0], "Release", lang):
+            return IntentHandlerMatch(match_type='persona:release',
+                                      match_data={"persona": self.active_persona},
+                                      skill_id="persona.openvoiceos",
+                                      utterance=utterances[0])
+
+    def match_medium(self, utterances: List[str], lang: str, message: Message) -> None:
+        return self.match_high(utterances, lang, message)
+
+    def match_low(self, utterances: List[str], lang: Optional[str] = None,
+                  message: Optional[Message] = None) -> Optional[IntentHandlerMatch]:
+        """
+        Recommended before fallback low
+
         Args:
             utterances (list):  list of utterances
             lang (string):      4 letter ISO language code
@@ -124,13 +213,23 @@ class PersonaService(PipelineStageMatcher, OVOSAbstractApplication):
         utt = message.data["answer"]
         self.speak(utt)
 
+    def handle_persona_summon(self, message):
+        persona = message.data["persona"]
+        if persona not in self.personas:
+            self.speak_dialog("unknown_persona")
+        else:
+            self.active_persona = persona
+
+    def handle_persona_release(self, message):
+        self.active_persona = None
+
 
 if __name__ == "__main__":
     b = PersonaService(FakeBus(),
                        config={"personas_path": "/home/miro/PycharmProjects/ovos-persona/personas"})
     print(b.personas)
 
-    print(b.match(["what is the speed of light"]))
+    print(b.match_low(["what is the speed of light"]))
 
     # The speed of light has a value of about 300 million meters per second
     # The telephone was invented by Alexander Graham Bell
